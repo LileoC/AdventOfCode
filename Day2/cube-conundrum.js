23a621484655e7b472bd08e665746f357048fe3a
LileoC <LileoC@proton.me>
2024-07-19 17:41:35 +0200

    get games

---
+++ Day2/cube-conundrum.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const filePath = "./input.txt";
+
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading the file:", err);
+    return;
+  }
+  const games = data.split("\n");
+  console.log(games);
+});
