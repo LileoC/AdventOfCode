a84b277f5ddb68a607dc3fd68620ff5625005779
LileoC <LileoC@proton.me>
2024-08-22 02:24:26 +0200

    Day 2 part 2

--- Day2/cube-conundrum.js
+++ Day2/cube-conundrum.js
@@ -35,70 +35,107 @@ fs.readFile(filePath, "utf8", (err, data) => {
     return result;
   }
 
-  function checkRounds(games) {
-    const limits = {
-      red: 12,
-      green: 13,
-      blue: 14,
-    };
+  // function checkRounds(games) {
+  //   const limits = {
+  //     red: 12,
+  //     green: 13,
+  //     blue: 14,
+  //   };
 
-    const validGameNumbers = [];
+  //   const validGameNumbers = [];
+
+  //   games.forEach((game) => {
+  //     let allRoundsValid = true;
+
+  //     game.rounds.forEach((round, index) => {
+  //       const counts = {
+  //         red: 0,
+  //         green: 0,
+  //         blue: 0,
+  //       };
+
+  //       round.forEach((item) => {
+  //         if (counts.hasOwnProperty(item.color)) {
+  //           counts[item.color] += item.count;
+  //         }
+  //       });
+
+  //       const isValid =
+  //         counts.red <= limits.red &&
+  //         counts.green <= limits.green &&
+  //         counts.blue <= limits.blue;
+
+  //       if (!isValid) {
+  //         allRoundsValid = false;
+  //       }
+
+  //       // console.log(
+  //       //   `Game ${game.gameNumber}, Round ${index + 1}: ${
+  //       //     isValid ? "Valid" : "Invalid"
+  //       //   }`
+  //       // );
+  //     });
+
+  //     if (allRoundsValid) {
+  //       validGameNumbers.push(game.gameNumber);
+  //     }
+  //   });
+
+  //   return validGameNumbers;
+  // }
+
+  // function extractNumbersAndSum(str) {
+  //   if (typeof str !== "string") {
+  //     throw new TypeError("Input must be a string");
+  //   }
+  //   return (str.match(/\d+/g) || [])
+  //     .map(Number)
+  //     .reduce((sum, num) => sum + num, 0);
+  // }
+
+  function calculateMinimumCubes(games) {
+    const minCubes = [];
 
     games.forEach((game) => {
-      let allRoundsValid = true;
+      const minCounts = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
 
-      game.rounds.forEach((round, index) => {
-        const counts = {
-          red: 0,
-          green: 0,
-          blue: 0,
-        };
-
+      game.rounds.forEach((round) => {
         round.forEach((item) => {
-          if (counts.hasOwnProperty(item.color)) {
-            counts[item.color] += item.count;
+          if (minCounts.hasOwnProperty(item.color)) {
+            minCounts[item.color] = Math.max(minCounts[item.color], item.count);
           }
         });
-
-        const isValid =
-          counts.red <= limits.red &&
-          counts.green <= limits.green &&
-          counts.blue <= limits.blue;
-
-        if (!isValid) {
-          allRoundsValid = false;
-        }
-
-        // console.log(
-        //   `Game ${game.gameNumber}, Round ${index + 1}: ${
-        //     isValid ? "Valid" : "Invalid"
-        //   }`
-        // );
       });
 
-      if (allRoundsValid) {
-        validGameNumbers.push(game.gameNumber);
-      }
+      minCubes.push(minCounts);
     });
-
-    return validGameNumbers;
+    return minCubes;
   }
 
-  function extractNumbersAndSum(str) {
-    if (typeof str !== "string") {
-      throw new TypeError("Input must be a string");
-    }
-    return (str.match(/\d+/g) || [])
-      .map(Number)
-      .reduce((sum, num) => sum + num, 0);
+  function calculatePower(cubeSet) {
+    return cubeSet.red * cubeSet.green * cubeSet.blue;
+  }
+
+  function sumPowers(minCubes) {
+    return minCubes.reduce((total, cubes) => {
+      return total + calculatePower(cubes);
+    }, 0);
   }
 
   const parsedData = parseData(data);
+  const minCubes = calculateMinimumCubes(parsedData);
+  const totalPower = sumPowers(minCubes);
 
-  //console.log(JSON.stringify(parsedData, null, 2));
-  const validGames = checkRounds(parsedData);
-  //console.log(validGames);
+  console.log(totalPower);
 
-  const sumOfNumbers = extractNumbersAndSum(validGames.join(" "));
-  console.log(sumOfNumbers);
+  // //console.log(JSON.stringify(parsedData, null, 2));
+  // const validGames = checkRounds(parsedData);
+  // //console.log(validGames);
+
+  // const sumOfNumbers = extractNumbersAndSum(validGames.join(" "));
+  // console.log(sumOfNumbers);
 });
